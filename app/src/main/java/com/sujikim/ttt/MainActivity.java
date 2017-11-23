2cd99d9355697593f252dd91398f5822589e4323
SujiKim6 <rlatnwl9595@naver.com>
2017-11-24 02:40:14 +0900

    try to make people to use camera

--- app/src/main/java/com/sujikim/ttt/MainActivity.java
+++ app/src/main/java/com/sujikim/ttt/MainActivity.java
@@ -1,17 +1,53 @@
 package com.sujikim.ttt;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button addClothes = (Button)findViewById(R.id.addClothesBtn);
+    Button addClothes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addClothes = (Button)findViewById(R.id.addClothesBtn);
+
+        addClothes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog  .setTitle("알림!")
+                        .setMessage("**조끼 입력시 지퍼를 열고 촬영해 주세요**")
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "사진을 촬영하세요", Toast.LENGTH_SHORT).show();
+                                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                startActivityForResult(i, 0);
+
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener(){
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "취소하였습니다", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                dialog.create();
+                dialog.show();
+            }
+        });
+
     }
 
 
