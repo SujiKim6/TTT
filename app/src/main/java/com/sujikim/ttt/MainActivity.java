5bf3f899fa3039ed326ad0dca798bf0f14ed9b74
SujiKim6 <rlatnwl9595@naver.com>
2017-11-29 03:48:06 +0900

    add permission from users

--- app/src/main/java/com/sujikim/ttt/MainActivity.java
+++ app/src/main/java/com/sujikim/ttt/MainActivity.java
@@ -2,187 +2,19 @@ package com.sujikim.ttt;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class MainActivity extends AppCompatActivity {
 
     Button addClothes;
     TextView degree;
-    MainActivity mContext;
-
-    // 현재 위치 받아오기 위해 변수 선언
-    LocationManager locationManager;
-    // 현재 GPS 사용유무
-    boolean isGPSEnabled = false;
-    // 네트워크 사용유무
-    boolean isNetworkEnabled = false;
-    // GPS 상태값
-    boolean isGetLocation = false;
-    Location location;
-    double lat; //위도
-    double lon; //경도
-
-    // 최소 GPS 정보 업데이트 거리 1000미터
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1000;
-    // 최소 업데이트 시간 1분
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
-
-    // 현재 위치를 가져오는 함수 선언
-    public Location getLocation() {
-        try {
-            locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
-            // GPS 정보 가져오기
-            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-            //현재 네트워크 상태 값 알아오기
-            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if (!isGPSEnabled && !isNetworkEnabled) {
-                // GPS와 네트워크 사용이 가능하지 않을때 소스 구현
-            }
-            else {
-                this.isGetLocation = true;
-                //네트워크 정보로부터 위치값 가져오기
-                if (isNetworkEnabled) {
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,MIN_TIME_BW_UPDATES,MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener);
-                    if (locationManager != null) {
-                        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (location != null) {
-                            // 위도 경도 저장
-                            lat = location.getLatitude();
-                            lon = location.getLongitude();
-                        }
-                    }
-                }
-                if (isGPSEnabled) {
-                    if (location == null) {
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener);
-                        if (locationManager != null) {
-                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            if (location != null) {
-                                lat = location.getLatitude();
-                                lon = location.getLongitude();
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return location;
-    }
-
-    private LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            //Toast("onLocationChanged");
-            getWeatherData(location.getLatitude(), location.getLongitude());
-        }
-
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-            //Toast("onStatusChanged");
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-            //Toast("onProviderEnabled");
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-            //Toast("onProviderDisabled");
-        }
-    };
-
-    private void getWeatherData (double lat, double lng) {
-        String url = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+
-                lon+"&units=metric&appid=10f6fc8cfbff125d7da532526029b553";
-        ReceiveWeatherTask receiverUseTask = new ReceiveWeatherTask();
-        receiverUseTask.execute(url);
-    }
-    private class ReceiveWeatherTask extends AsyncTask<String, Void, JSONObject> {
-        @Override
-        protected  void onPreExecute() {
-            super.onPreExecute();
-        }
-        @Override
-        protected JSONObject doInBackground(String... datas) {
-            try {
-                HttpURLConnection conn = (HttpURLConnection) new URL(datas[0]).openConnection();
-                conn.setConnectTimeout(10000);
-                conn.setReadTimeout(10000);
-                conn.connect();
-
-                if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    InputStream is = conn.getInputStream();
-                    InputStreamReader reader = new InputStreamReader(is);
-                    BufferedReader in = new BufferedReader(reader);
-
-                    String readed;
-                    while ((readed = in.readLine()) != null) {
-                        JSONObject jObject = new JSONObject(readed);
-//                        String result = jObject.getJSONArray("weather").getJSONObject(0).getString("icon");
-                        return jObject;
-                    }
-                } else {
-                    return null;
-                }
-                return null;
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-        @Override
-        protected void onPostExecute(JSONObject result) {
-//        Log.i(TAG, result.toString());
-            if (result != null) {
-                String maxTemp = "";
-                String minTemp = "";
-                String description = "";
-
-                try {
-                    minTemp = result.getJSONObject("main").getString("temp_min");
-                    maxTemp = result.getJSONObject("main").getString("temp_max");
-                    description = result.getJSONArray("weather").getJSONObject(0).getString("description");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                final String msg = description + "최저" +minTemp + "/ 최고:"+maxTemp;
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
