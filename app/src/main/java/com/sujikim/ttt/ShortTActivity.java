8fe2c17164fa599cd5676461a6328099c01f891f
Suyoung Kim <usingname@naver.com>
2017-11-30 17:08:40 +0900

    my closet long,shortt

--- app/src/main/java/com/sujikim/ttt/ShortTActivity.java
+++ app/src/main/java/com/sujikim/ttt/ShortTActivity.java
@@ -2,6 +2,7 @@ package com.sujikim.ttt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ShortTActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class ShortTActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_short_t);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
