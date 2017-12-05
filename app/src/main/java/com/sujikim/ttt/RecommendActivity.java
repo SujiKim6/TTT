00cf4e15629c8fb84b4915e1caa37a729c374c35
Suyoung Kim <usingname@naver.com>
2017-12-06 06:48:25 +0900

    recommendView grid

--- app/src/main/java/com/sujikim/ttt/RecommendActivity.java
+++ app/src/main/java/com/sujikim/ttt/RecommendActivity.java
@@ -2,6 +2,7 @@ package com.sujikim.ttt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RecommendActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class RecommendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
