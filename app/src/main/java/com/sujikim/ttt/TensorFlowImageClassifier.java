3b0ba7ac8501195eb3a7e6dc6a7845b95bdbe8fd
SujiKim6 <rlatnwl9595@naver.com>
2017-12-10 13:27:00 +0900

    Change Show part

--- app/src/main/java/com/sujikim/ttt/TensorFlowImageClassifier.java
+++ app/src/main/java/com/sujikim/ttt/TensorFlowImageClassifier.java
@@ -46,7 +46,7 @@ public class TensorFlowImageClassifier implements Classifier {
     // Only return this many results with at least this confidence.
     private static final int MAX_RESULTS = 3;
 //    private static final float THRESHOLD = 0.1f;
-private static final float THRESHOLD = 0;
+    private static final float THRESHOLD = 0;
 
     // Config values.
     private String inputName;
